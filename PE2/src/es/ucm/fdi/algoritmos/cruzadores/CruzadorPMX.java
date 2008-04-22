f682352dcd54771246ff42021647b9a06c5b5432
pablete.im <pablete.im@92360ab8-504b-0410-8b0d-81f058a70e5b>
2008-04-22 14:11:35 +0000

    PMX terminado Busquedas mejoradas

--- PE2/src/es/ucm/fdi/algoritmos/cruzadores/CruzadorPMX.java
+++ PE2/src/es/ucm/fdi/algoritmos/cruzadores/CruzadorPMX.java
@@ -5,6 +5,7 @@ import es.ucm.fdi.cromosomas.CromosomaEnteroViajante;
 import es.ucm.fdi.evaluadores.Evaluador;
 import es.ucm.fdi.genes.Gen;
 import es.ucm.fdi.genes.GenEntero;
+import es.ucm.fdi.utils.Busquedas;
 import es.ucm.fdi.utils.MyRandom;
 
 public class CruzadorPMX implements Cruzador {
@@ -19,7 +20,7 @@ public class CruzadorPMX implements Cruzador {
 		//creamos el array de genes de los hijos
 		GenEntero[] genesHijo1 = new GenEntero[padre1.getNumeroGenes()];
 		GenEntero[] genesHijo2 = new GenEntero[padre1.getNumeroGenes()];
-		
+
 		//para cada gen del cromosoma:
 		for(int i = 0; i<padre1.getNumeroGenes(); i++){
 			
@@ -37,32 +38,138 @@ public class CruzadorPMX implements Cruzador {
 				mayor = corte1;
 			}
 			
-			//Hallamos la longitud de los segmentos
-			int tamañoSegmentos = mayor - menor + 1;
-			int[] segmentoPadre1 = new int[tamañoSegmentos];
-			int[] segmentoPadre2  = new int[tamañoSegmentos];
-			
 			//Extraemos los genes de los padres
 			Gen[] genesPadre1 = padre1.getGenes();
 			Gen[] genesPadre2 = padre2.getGenes();
 			
-			//TODO
-			int[] codificacionGenI = new int[((GenEntero)genesPadre1[i]).getGen()[i]];
-			//Nos quedamos con los segmentos situados entre los puntos de corte
-			for (int j = menor; j<=mayor; j++){
-				segmentoPadre1[j] = ((GenEntero)genesPadre1[i]).getGen()[j];
-				segmentoPadre2[j] = ((GenEntero)genesPadre2[i]).getGen()[j];
-			}
+			int longitudGen = ((GenEntero)genesPadre1[i]).getLongitud();
+			int[] codificacionGenPadre1I = new int[longitudGen];
+			int[] codificacionGenPadre2I = new int[longitudGen];
+			
+			//Guardamos las codificaciones de los genes de los padres en un array de enteros
+			codificacionGenPadre1I = ((GenEntero)genesPadre1[i]).getGen();
+			codificacionGenPadre2I = ((GenEntero)genesPadre2[i]).getGen();
+		
+			int[] codificacionHijo1I = new int[longitudGen];
+			int[] codificacionHijo2I = new int[longitudGen];
 			
 			//Intercambiamos los segmentos de los dos padres
-			for(int j = menor; j<mayor; j++){
+			for(int j = menor; j<=mayor; j++){
+				codificacionHijo1I[j] = codificacionGenPadre2I[j];
+				codificacionHijo2I[j] = codificacionGenPadre1I[j];
+			}
+			//Especificamos las posiciones sin valor de los progenitores orignales que no planteen conflicto
+			//Recorremos todo el gen hijo en busca de algun valor repetido
+			
+			//Parte del gen por debajo del corte menor
+			for(int j=0;j<menor;j++){
+				int k=menor;
+				boolean encontrado = false;
+				//Hijo1
+				while(!encontrado && k<=mayor){
+					if(codificacionGenPadre1I[j]==codificacionHijo1I[k]){ //Conflicto
+						//Tenemos que comprobar que el numero de la pareja no este ya en el segmento del hijo
+						//Buscamos la pareja en el segmento
+						int elem = codificacionHijo2I[k];
+						int indice = Busquedas.buscar(elem, codificacionHijo1I, menor, mayor);
+						while(indice != -1){
+							elem = codificacionHijo2I[indice];
+							indice = Busquedas.buscar(elem, codificacionHijo1I, menor, mayor);
+								
+						}
+						codificacionHijo1I[j] = elem;
+						encontrado = true;
+					}
+					k++;
+				}
+				if(!encontrado)
+					codificacionHijo1I[j] = codificacionGenPadre1I[j];
 				
+				k=menor;
+				encontrado = false;
+				//Hijo2
+				while(!encontrado && k<=mayor){
+					if(codificacionGenPadre2I[j]==codificacionHijo2I[k]){ //Conflicto
+						//Tenemos que comprobar que el numero de la pareja no este ya en el segmento del hijo
+						//Buscamos la pareja en el segmento
+						int elem = codificacionHijo1I[k];
+						int indice = Busquedas.buscar(elem, codificacionHijo2I, menor, mayor);
+						while(indice!=-1){
+							elem = codificacionHijo1I[indice];
+							indice = Busquedas.buscar(elem, codificacionHijo2I, menor, mayor);
+						}
+						codificacionHijo2I[j] = elem;
+						encontrado = true;
+					}
+					k++;
+				}
+				if(!encontrado)
+					codificacionHijo2I[j] = codificacionGenPadre2I[j];
 			}
 			
+			//Parte del gen por encima del corte mayor
+			for(int j=mayor+1;j<longitudGen;j++){
+				int k=menor;
+				boolean encontrado = false;
+				//Hijo1
+				while(!encontrado && k<=mayor){
+					if(codificacionGenPadre1I[j]==codificacionHijo1I[k]){ //Conflicto
+						//Tenemos que comprobar que el numero de la pareja no este ya en el segmento del hijo
+						//Buscamos la pareja en el segmento
+						int elem = codificacionHijo2I[k];
+						int indice = Busquedas.buscar(elem, codificacionHijo1I, menor, mayor);
+						while(indice != -1){
+							elem = codificacionHijo2I[indice];
+							indice = Busquedas.buscar(elem, codificacionHijo1I, menor, mayor);
+								
+						}
+						codificacionHijo1I[j] = elem;
+						encontrado = true;
+					}
+					k++;
+				}
+				if(!encontrado)
+					codificacionHijo1I[j] = codificacionGenPadre1I[j];
+				
+				k=menor;
+				encontrado = false;
+				//Hijo2
+				while(!encontrado && k<=mayor){
+					if(codificacionGenPadre2I[j]==codificacionHijo2I[k]){ //Conflicto
+						//Tenemos que comprobar que el numero de la pareja no este ya en el segmento del hijo
+						//Buscamos la pareja en el segmento
+						int elem = codificacionHijo1I[k];
+						int indice = Busquedas.buscar(elem, codificacionHijo2I, menor, mayor);
+						while(indice!=-1){
+							elem = codificacionHijo1I[indice];
+							indice = Busquedas.buscar(elem, codificacionHijo2I, menor, mayor);
+						}
+						codificacionHijo2I[j] = elem;
+						encontrado = true;
+					}
+					k++;
+				}
+				if(!encontrado)
+					codificacionHijo2I[j] = codificacionGenPadre2I[j];
+			}
+			
+			GenEntero genHijo1I = new GenEntero(longitudGen);
+			GenEntero genHijo2I = new GenEntero(longitudGen);
+			
+			genHijo1I.setGen(codificacionHijo1I);
+			genHijo2I.setGen(codificacionHijo2I);
+			
+			genesHijo1[i] = genHijo1I;
+			genesHijo2[i] = genHijo2I;			
+				
 		}
+		hijo1.setGenes(genesHijo1, evaluador);
+		hijo2.setGenes(genesHijo2, evaluador);
 		
-		 
-		return null;
+		Cromosoma[] hijos = new Cromosoma[2];
+		hijos[0] = hijo1;
+		hijos[1] = hijo2;
+		return hijos;
 	}
 
 }
--- PE2/src/es/ucm/fdi/utils/Busquedas.java
+++ PE2/src/es/ucm/fdi/utils/Busquedas.java
@@ -2,19 +2,25 @@ package es.ucm.fdi.utils;
 
 public class Busquedas {
 
-	//devuelve el indice de la primera ocurrencia, o -1 si no lo ha encontrado
-	//TODO generalizar para buscar entre dos indices
-	public static int buscar (int elemento, int[] array){
+	//devuelve el indice de la primera ocurrencia entre menor y mayor, o -1 si no lo ha encontrado
+	public static int buscar (int elemento, int[] array, int indiceMenor, int indiceMayor){
 		int indice = -1;
-		int i = 0;
+		int i = indiceMenor;
 		boolean encontrado = false;
-		while (!encontrado && i< array.length){
-			if (elemento == array[i]){
-				encontrado = true;
-				indice = i;
+		if (indiceMayor <= array.length){
+			while (!encontrado && i<= indiceMayor){
+				if (elemento == array[i]){
+					encontrado = true;
+					indice = i;
+				}
+				i++;
 			}
-			i++;
 		}
 		return indice;
+	}
+	
+	//devuelve el indice de la primera ocurrencia entre 0 y n-1, o -1 si no lo ha encontrado
+	public static int buscar (int elemento, int[] array){
+		return buscar (elemento, array, 0, array.length - 1);
 	}
 }
