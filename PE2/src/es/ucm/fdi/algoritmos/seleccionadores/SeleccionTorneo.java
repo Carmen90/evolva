d8d3b94c9c23d85320dc8a7f01ddabf7bee9ef58
nachorubiodiaz <nachorubiodiaz@92360ab8-504b-0410-8b0d-81f058a70e5b>
2008-04-26 17:13:03 +0000

    Añadida variable statica final numContrincantes

--- PE2/src/es/ucm/fdi/algoritmos/seleccionadores/SeleccionTorneo.java
+++ PE2/src/es/ucm/fdi/algoritmos/seleccionadores/SeleccionTorneo.java
@@ -5,24 +5,25 @@ import es.ucm.fdi.evaluadores.Evaluador;
 import es.ucm.fdi.utils.MyRandom;
 
 public class SeleccionTorneo implements Seleccionador {
-
+	private static final int numContrincantes = 3;
+	
 	public Cromosoma[] seleccion(Cromosoma[] poblacionInicial, Evaluador e) {
 		Cromosoma[] poblacionAux = new Cromosoma[poblacionInicial.length];
-		Cromosoma[] elegidos = new Cromosoma[3];
+		Cromosoma[] elegidos = new Cromosoma[numContrincantes];
 		Cromosoma campeon = null;
 		int indice = 0;
 		
 		while(indice < poblacionInicial.length){
 			
 			//Generamos 3 numeros aleatorios y extraemos 3 cromosomas que competiran en el torneo
-			for(int i = 0; i<3; i++){
+			for(int i = 0; i<numContrincantes; i++){
 				int numGenerado = MyRandom.aleatorioEntero(0, poblacionInicial.length);
 				elegidos[i] = poblacionInicial[numGenerado];
 			}
 			
 			//El cromosoma campeon sera el mejor de los 3
 			campeon = elegidos[0];
-			for(int i = 1; i < 3; i++){
+			for(int i = 1; i < numContrincantes; i++){
 				if(elegidos[i].getAptitud()>campeon.getAptitud())
 					campeon = elegidos[i];
 			}
