d866a8e3e5c378089e00715328e037d885ecc9a2
nachorubiodiaz <nachorubiodiaz@92360ab8-504b-0410-8b0d-81f058a70e5b>
2008-05-05 19:44:30 +0000

    Implementado escalado lineal

--- PE2/src/es/ucm/fdi/algoritmos/seleccionadores/SeleccionRuleta.java
+++ PE2/src/es/ucm/fdi/algoritmos/seleccionadores/SeleccionRuleta.java
@@ -3,9 +3,11 @@ package es.ucm.fdi.algoritmos.seleccionadores;
 import es.ucm.fdi.cromosomas.Cromosoma;
 import es.ucm.fdi.evaluadores.Evaluador;
 import es.ucm.fdi.utils.Ordenacion;
+import es.ucm.fdi.utils.Poblacion;
 
 public class SeleccionRuleta implements Seleccionador{
-
+	
+	public static final double C = 1.5; 
 	//precondicion!! la poblacion inicial debe estar ordenada
 	public Cromosoma[] seleccion(Cromosoma[] poblacionInicial, Evaluador e) {
 		int tamañoPoblacion = poblacionInicial.length;
@@ -31,8 +33,16 @@ public class SeleccionRuleta implements Seleccionador{
 	public Cromosoma[] generarSegmentos(Cromosoma[] poblacion, Evaluador evaluador, boolean mejora) {
 		double puntAcumulada = 0.0; //puntuacion acumulada
 		double sumaAptitud = 0.0; //suma de la aptitud
-		
 		int tamañoPoblacion = poblacion.length;
+		
+		double[] aptitudesEscaladas = new double[poblacion.length];
+		double a = (C-1)*Poblacion.mediaPoblacionInstantanea(poblacion)/(Poblacion.mejorPoblacionInstantanea(poblacion, evaluador).getAptitud()-Poblacion.mediaPoblacionInstantanea(poblacion));
+		double b = Poblacion.mediaPoblacionInstantanea(poblacion)*(1-a);
+		
+		for(int i = 0; i<tamañoPoblacion; i++){
+			aptitudesEscaladas[i] = a*evaluador.evaluaAptitud(poblacion[i])+b;
+		}
+		
 		
 		//ordenamos la poblacion de peor a mejor aptitud.
 		poblacion = Ordenacion.sortSelectionIndividuals(poblacion, evaluador);
@@ -46,7 +56,10 @@ public class SeleccionRuleta implements Seleccionador{
 		//si el problema es de minimizacion, debemos asignar mejores puntuaciones a los individuos con
 		//menor aptitud luego transformamos el problema de minimizacion en un problema de maximizacion
 		double[] aptitudesMaximizadas = new double[aptitudesPositivas.length];
-		aptitudesMaximizadas = evaluador.transformarAptitudesAMaximizacion(aptitudesPositivas);
+		if(mejora)
+			aptitudesMaximizadas = evaluador.transformarAptitudesAMaximizacion(aptitudesEscaladas);
+		else 
+			aptitudesMaximizadas = evaluador.transformarAptitudesAMaximizacion(aptitudesPositivas);
 		aptitudesPositivas = aptitudesMaximizadas;
 		
 		//calculamos la suma de las aptitudes
