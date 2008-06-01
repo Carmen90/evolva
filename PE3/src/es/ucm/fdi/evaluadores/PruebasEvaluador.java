bdc693eddcea6993912e8ab778a4d7b3cb100372
pablete.im <pablete.im@92360ab8-504b-0410-8b0d-81f058a70e5b>
2008-06-01 15:58:33 +0000

    pruebas de evaluacion mejoradas

--- PE3/src/es/ucm/fdi/evaluadores/PruebasEvaluador.java
+++ PE3/src/es/ucm/fdi/evaluadores/PruebasEvaluador.java
@@ -5,11 +5,12 @@ import es.ucm.fdi.cromosomas.Cromosoma;
 public class PruebasEvaluador {
 
 	public static void main(String[] args) {
-		Evaluador evaluador = new EvaluadorHormigas();
+		Evaluador evaluador = new EvaluadorHormigas(true);
 		
 		//PRUEBA DE GENERACION ALEATORIA DE CROMOSOMAS:
-		Cromosoma c = evaluador.generarCromosomaAleatorio();
+		//Cromosoma c = evaluador.generarCromosomaAleatorio();
 		//Cromosoma c = evaluador.generarCromosomaFijo();
+		Cromosoma c = ((EvaluadorHormigas)evaluador).generarCromosomaSolucion();
 		System.out.println("--->PRUEBA DE GENERACION ALEATORIA DE CROMOSOMAS");
 		System.out.println("El cromosoma aleatorio generado es el siguiente: ");
 		System.out.println(c.getFenotipo());
@@ -21,7 +22,15 @@ public class PruebasEvaluador {
 		c.setAptitud(evaluador.evaluaAptitud(c));
 		System.out.println("La aptitud del cromosoma aleatorio es: ");
 		System.out.println(c.getAptitud());
-
+		
+		boolean[][] pasosDados = ((EvaluadorHormigas)evaluador).getPasos();
+		for (int i = 0; i<pasosDados.length; i++){
+			for (int j = 0; j < pasosDados[i].length; j++){
+				if (pasosDados[i][j]) System.out.print ("# ");
+				else System.out.print(". ");
+			}
+			System.out.println();
+		}
 	}
 
 }
